4699e25c7931dca97c93689062402a1ada1d0ade
Ras Brits <ras@fullfacing.com>
2018-09-25 00:11:09 +0200

    Added some morale boosters

--- src/screens/GameDeck/index.js
+++ src/screens/GameDeck/index.js
@@ -4,14 +4,27 @@ import { Button } from 'react-native-paper'
 
 import activeDeck from '../../containers/active-deck'
 import players from '../../containers/players'
+import MoraleBoosters from '../../config/moraleBoosters'
 
 class GameDeck extends Component {
     state = {
       text: '',
       isGameDone: false,
       bgColor: '#ffb677',
+      currentIndex: 0,
       color: new Animated.Value(0),
-      btnColor: new Animated.Value(0)
+      btnColor: new Animated.Value(0),
+      overlay: {
+        height: new Animated.Value(0),
+        width: new Animated.Value(0),
+        rad: new Animated.Value(0)
+      },
+      mask: {
+        height: new Animated.Value(0),
+        width: new Animated.Value(0),
+        rad: new Animated.Value(0)
+      },
+      animating: false
     }
 
     async componentDidMount () {
@@ -27,12 +40,14 @@ class GameDeck extends Component {
     handleNextCard = async () => {
       try {
         const card = await activeDeck.getCard()
-
         this.setState({
-          text: card ? card(players.state.players) : '',
           isGameDone: !card,
           bgColor: this.getRandomColor()
-        }, () => this.doTransition())
+        }, () => {
+          this.changeLabel(card ? card(players.state.players) : '')
+          this.interimLabel()
+          this.doTransition()
+        })
       } catch (error) {
         console.log(error)
         this.handleBack()
@@ -40,22 +55,116 @@ class GameDeck extends Component {
     }
 
     doTransition = () => {
-      Animated.timing(this.state.color, {
-        toValue: Math.floor(Math.random() * 256),
-        duration: 400
-      }).start();
-      Animated.timing(this.state.btnColor, {
-        toValue: Math.floor(Math.random() * 256),
-        duration: 400
-      }).start();
+      Animated.sequence([
+        this.openMask(),
+        Animated.delay(570),
+        this.hideOverlay(),
+        this.hideMask(),
+        this.openOverlay(),
+      ]).start()
+    }
+
+    openOverlay = () =>{
+      const {width, height, rad} = this.state.overlay
+      const duration = 300
+      return Animated.parallel([
+        Animated.timing(this.state.color, {
+          toValue: Math.floor(Math.random() * 256),
+          duration
+        }),
+        Animated.timing(this.state.btnColor, {
+          toValue: Math.floor(Math.random() * 256),
+          duration
+        }),
+        Animated.timing(height, {
+          toValue: 1000,
+          duration
+        }),
+        Animated.timing(width, {
+          toValue: 1000,
+          duration
+        }),
+        Animated.timing(rad, {
+          toValue: 500,
+          duration
+        })
+      ])
+    }
+
+    hideOverlay = () =>{
+      const {width, height, rad} = this.state.overlay
+      return Animated.parallel([
+        Animated.timing(height, {
+          toValue: 0,
+          duration: 1
+        }),
+        Animated.timing(width, {
+          toValue: 0,
+          duration: 1
+        }),
+        Animated.timing(rad, {
+          toValue: 0,
+          duration: 1
+        })
+      ])
+    }
+
+    openMask = () =>{
+      const {width, height, rad} = this.state.mask
+      const duration = 300
+      return Animated.parallel([
+              Animated.timing(height, {
+                toValue: 1000,
+                duration
+              }),
+              Animated.timing(width, {
+                toValue: 1000,
+                duration
+              }),
+              Animated.timing(rad, {
+                toValue: 500,
+                duration
+              })
+            ])
+    }
+
+    hideMask = () =>{
+      const {width, height, rad} = this.state.mask
+      return Animated.parallel([
+        Animated.timing(height, {
+          toValue: 0,
+          duration: 1
+        }),
+        Animated.timing(width, {
+          toValue: 0,
+          duration: 1
+        }),
+        Animated.timing(rad, {
+          toValue: 0,
+          duration: 1
+        })
+      ])
+    }
+
+    interimLabel = () =>{
+      setTimeout(() => {
+        this.setState({
+          text: MoraleBoosters[this.state.currentIndex],
+          animating : true
+        })
+      }, 200);
+    }
+
+    changeLabel = (text) =>{
+      setTimeout(() => {
+        this.setState({text, animating : false, currentIndex: this.state.currentIndex + 1})
+      }, 1000);
     }
 
     handleBack = () => this.props.navigation.goBack()
 
     render () {
-      const { text, isGameDone, bgColor } = this.state
-
-      const currentText = isGameDone ? 'Done! Thanks for playing' : text
+      const { text, isGameDone, animating, currentIndex, overlay, mask } = this.state
       const btnText = isGameDone ? 'New game' : 'Next'
       const btnHandler = isGameDone ? this.handleBack : this.handleNextCard
 
@@ -69,10 +178,29 @@ class GameDeck extends Component {
         outputRange: ['rgba(20, 100, 211, 1)', 'rgba(200, 29, 10, 1)']
       });
 
+      let overlayStyles = {
+        height: overlay.height,
+        width: overlay.width,
+        borderRadius: overlay.rad,
+        backgroundColor: color
+      }
+
+      let maskStyles = {
+        height: mask.height,
+        width:mask.width,
+        borderRadius: mask.rad,
+        backgroundColor: "white"
+      }
+
       return (
-        <Animated.View style={{ flex: 1 , backgroundColor: color}}>
+        <Animated.View style={{ flex: 1 , backgroundColor: 'white', overflow: 'hidden'}}>
+          <Animated.View style={[styles.overlay, overlayStyles]}></Animated.View>
+          <Animated.View style={[styles.overlay, maskStyles]}></Animated.View>
           <View style={styles.textContainer}>
-            <Text style={styles.text}>{currentText}</Text>
+            {
+              currentIndex === 0 ? null :
+              <Animated.Text style={[styles.text, {color: animating ? color : "white" }]}>{text}</Animated.Text>
+            }
           </View>
           <Button
             mode='outlined'
@@ -86,7 +214,8 @@ class GameDeck extends Component {
 const styles = StyleSheet.create({
   textContainer: { flex: 1, alignItems: 'center', justifyContent: 'center', margin: 10 },
   text: { color: '#fff', textAlign: 'center', fontSize: 30 },
-  btn: { margin: 10 }
+  btn: { margin: 10 },
+  overlay:{ backgroundColor: 'black', position: 'absolute', bottom: -250, left: -290}
 })
 
 export default GameDeck
